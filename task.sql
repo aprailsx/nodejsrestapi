d4b8f4176861846164d35760b49fb7db7c5b3d34
Ahries Panopio <a.panopio@iparentportal.com>
2018-08-29 10:54:34 +0800

    add books table

--- task.sql
+++ task.sql
@@ -1,24 +1,40 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 05, 2016 at 07:23 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
+-- Host: localhost:8889
+-- Generation Time: Aug 29, 2018 at 04:53 AM
+-- Server version: 5.6.38
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Database: `task`
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- --------------------------------------------------------
 
 --
--- Database: `demo`
+-- Table structure for table `books`
 --
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(100) NOT NULL,
+  `book_author` varchar(100) NOT NULL,
+  `book_status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `book_title`, `book_author`, `book_status`) VALUES
+(3, 'Tesla Files', 'History Channel', 'available'),
+(4, 'Alien', 'National Geographic', 'available'),
+(6, '50 Shades', 'Michael Gray', 'Not available');
 
 -- --------------------------------------------------------
 
@@ -26,11 +42,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `task`
 --
 
-CREATE TABLE IF NOT EXISTS `task` (
+CREATE TABLE `task` (
   `Id` varchar(50) NOT NULL,
   `Title` varchar(500) DEFAULT NULL,
-  `Status` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
+  `Status` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -39,11 +54,35 @@ CREATE TABLE IF NOT EXISTS `task` (
 
 INSERT INTO `task` (`Id`, `Title`, `Status`) VALUES
 ('1', 'Go to Market tomorrow', 'done'),
-('2', 'Email to manager', 'pending'),
+('2', NULL, NULL),
 ('3', 'Push code to GitHub', 'done'),
 ('4', 'Go For Running', 'done'),
-('5', 'Go to Movie', 'pending');
+('5', 'Go to Movie', 'pending'),
+('6', NULL, 'done');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`),
+  ADD UNIQUE KEY `book_id` (`book_id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
